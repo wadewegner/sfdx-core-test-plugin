eda6233c4bda2b3ff3bb7b691fb1093a5a43b1a6
Wade Wegner <wade.wegner@gmail.com>
2018-03-31 14:53:41 -0500

    Initial push

---
+++ README.md
@@ -0,0 +1,3 @@
+# sfdx-core-test-plugin
+
+Just playing around with the new Salesforce DX Core Libraries in an OCLIF-based plugin.
---
+++ src/commands/hello/org.ts
@@ -0,0 +1,69 @@
+import { flags } from '@oclif/command';
+import { join } from 'path';
+import { SfdxCommand, core } from '@salesforce/command';
+
+export default class Org extends SfdxCommand {
+
+  public static description = 'command message';
+  protected static requiresUsername = true;
+  protected static supportsDevhubUsername = true;
+  protected static requiresProject = false;
+
+  public async run(): Promise<any> {
+
+    // SfdxCommand
+    this.ux.styledHeader('SfdxCommand');
+
+    // Dev Hub
+    await this.hubOrg.refreshAuth();
+    this.ux.log('refreshed devhub org auth');
+
+    let devHubUsername = this.hubOrg.getConnection().getUsername();
+    this.ux.log('devHubUsername', devHubUsername);
+
+    let devHubOrgId = this.hubOrg.getOrgId();
+    this.ux.log('devHubOrgId', devHubOrgId);
+
+    let devHubOrgConnectionOptions = this.hubOrg.getConnection().getAuthInfo().getConnectionOptions();
+    this.ux.log('devHubOrgConnectionOptions', devHubOrgConnectionOptions);
+
+    let devHubOrgConfigAggregator = this.hubOrg.getConfigAggregator();
+    let devHubOrgConfigInfo = devHubOrgConfigAggregator.getConfigInfo();
+    this.ux.log('devHubOrgConfigInfo', devHubOrgConfigInfo);
+
+    // Default Org
+    await this.org.refreshAuth();
+    this.ux.log('refreshed org auth');
+
+    let username = this.org.getConnection().getUsername();
+    this.ux.log('username', username);
+
+    let orgId = this.org.getOrgId();
+    this.ux.log('orgId', orgId);
+
+    let orgConnectionOptions = this.org.getConnection().getAuthInfo().getConnectionOptions();
+    this.ux.log('orgConnectionOptions', orgConnectionOptions);
+
+    let orgConfigAggregator = this.org.getConfigAggregator();
+    let orgConfigInfo = orgConfigAggregator.getConfigInfo();
+    this.ux.log('orgConfigInfo', orgConfigInfo);
+
+    // core
+    this.ux.styledHeader('core');
+
+    let devHubOrgOptions = core.OrgUsersConfig.getOptions(devHubOrgId);
+    this.ux.log('devHubOrgOptions', devHubOrgOptions);
+
+    let orgOptions = core.OrgUsersConfig.getOptions(orgId);
+    this.ux.log('orgOptions', orgOptions);
+
+    let usernameAliasFileName = await core.Aliases.getFileName();
+    this.ux.log('usernameAliasFileName', usernameAliasFileName);
+
+    // Return an object to be displayed with --json
+    return { devHubUsername, devHubOrgId, devHubOrgConnectionOptions, devHubOrgConfigInfo, 
+      username, orgId, orgConnectionOptions, orgConfigInfo,
+      devHubOrgOptions, orgOptions, usernameAliasFileName 
+    };
+  }
+}
